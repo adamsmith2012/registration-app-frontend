3d4aa1657d206129e663f5bb2d8cbe67d1031edb
588870 <adamsmith@c1mrq060hf4h.attlocal.net>
2017-06-02 09:37:06 -0700

    Possible mobile fix

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
 app.get('/*', function(req, res) {
   res.sendfile('public/index.html');
 });
